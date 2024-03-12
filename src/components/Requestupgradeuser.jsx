ff88b4f3fd4413702da86d2e28a91579f871dcf8
Taksporn Phonpimpa <154772030+Taksaphorn@users.noreply.github.com>
2024-03-12 13:47:24 +0700

    Add files via upload

---
+++ src/components/Requestupgradeuser.jsx
@@ -0,0 +1,31 @@
+import './form.css';
+
+const RequestUpgradeUser = (props) => {
+    return (
+        <div className="RequestUpgradeUser">
+            <div className='h2'>
+                <h2>ข้อมูลสิทธิ์ปัจจุบัน</h2>
+            </div>
+            <div className='p'>
+            <p>รหัสพนักงาน : 123xxxxxxxx</p>
+            <p>ชื่อ-นามสกุล : นายเอ รักสงบ</p>
+            <p>สังกัด : xxx</p>
+            <p>สิทธิ์ : ดูได้อย่างเดียว</p>
+            </div>
+             <div className="user-department">
+                    <select className="list2">
+                    <option  value="" disabled selected hidden classname='hl'>กำหนดสิทธิ์ผู้ใช้</option>
+                    <option value="admin">มีสิทธิ์ดูหน้าเว็บได้อย่างเดียว</option>
+                    <option value="">มีสิทธิ์ดูหน้าเว็บได้/export ข้อมูลดิบได้</option>
+                    <option value="">มีสิทธิ์ดูหน้าเว็บได้/export ข้อมูลดิบได้/และสามารถใช้ฟังก์ชัน AI</option>
+                    <option value="">มีสิทธิ์ดูหน้าเว็บได้/เพิ่มข้อมูลหม้อแปลงได้</option>
+                    <option value="">พนักงานกฟภ.</option>
+                    </select>
+             </div>
+             <button type="submit" className="send-button">ยืนยัน</button>
+             <button type="cancel" className="cancel-button">ยกเลิก</button>
+        </div>
+    )
+}
+
+export default RequestUpgradeUser
